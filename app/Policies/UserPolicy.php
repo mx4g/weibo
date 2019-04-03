787dab13cbebfc52a302dd309b5d5f5a77fdd249
Qidong <522293522@qq.com>
2019-04-03 14:06:43 +0000

    管理员可删除用户

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,9 +9,16 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
-
+    //1.只有当前登录用户为自己才能执行编辑操作
     public function update(User $currentUser, User $user)
     {
         return $currentUser->id === $user->id;
+    }
+
+    //1.只有当前登录用户为管理员才能执行删除操作；
+    //2.删除的用户对象不是自己（即使是管理员也不能自己删自己）。
+    public function destroy(User $currentUser, User $user)
+    {
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
 }
