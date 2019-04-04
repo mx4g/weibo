486b9212811f0766c48f748b6838cb3158bd3b73
Qidong <522293522@qq.com>
2019-04-04 14:47:46 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,18 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }   
 
+    //通过 followers 来获取粉丝关系列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    //通过 followings 来获取用户关注人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
     /**
      * The attributes that are mass assignable.
      * 表示可以写入的属性
@@ -68,5 +80,29 @@ class User extends Authenticatable
     {
         return $this->statuses()
                     ->orderBy('created_at', 'desc');
+    }
+
+    //关注
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    //取消关注
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    //当前登录的用户 A 是否关注了用户 B
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
     }
 }
